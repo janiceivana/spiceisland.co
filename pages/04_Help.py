4f17c59e6aab4cf4d123315db2251b3507ca749e
Janice Ivana <jiva0001@student.monash.edu>
2024-06-20 16:10:59 +1000

    Update 04_Help.py

--- pages/04_Help.py
+++ pages/04_Help.py
@@ -5,8 +5,8 @@ import streamlit as st
 st.markdown('#### Need Help?')
 st.image("help.jpeg")
 
-url = "https://github.com/janiceivana/streamlit_fit3164/blob/main/User_guide.pdf"
-st.write("Click here for [User Guide](%s)" % url)
+# url = "https://github.com/janiceivana/streamlit_fit3164/blob/main/User_guide.pdf"
+# st.write("Click here for [User Guide](%s)" % url)
 
 
 st.write("Please contact our team to help you troubleshoot")
