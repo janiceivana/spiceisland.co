7b60b0a728402917f991a260a764b6ec053cd070
Janice Ivana <jiva0001@student.monash.edu>
2024-06-20 15:56:47 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,19 +12,6 @@ st.subheader("Getting Started")
 st.write("Start of by uploading your data to get your pricing analysis and strategies!")
 
 st.subheader("Meet our team members!")
-col1, col2, col3 = st.columns([1,1,1])
-# Using 'with' notation:
-with col1:
-    st.write("Janice Ivana - 32959796")
-    st.image("IMG-20240108-WA0049.jpg")
-
-with col2:
-    st.write("Jiayi Wang (Wendy) - 31513638")
-    st.image("Image (1).jpeg")
-    
-with col3:
-    st.write("Nguyen Pham (Chris) - 30750814")
-    st.image("pfp.jpeg")
 
 
 
